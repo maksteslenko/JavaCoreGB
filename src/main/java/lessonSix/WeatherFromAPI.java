679809c906482d2ca0642326b0a4f2a05c56e54b
maksteslenko <mks.tesl@gmail.com>
2022-06-19 16:53:34 +0300

    Шестая домашка по Java Core (Работа с сетью)

---
+++ src/main/java/lessonSix/WeatherFromAPI.java
@@ -0,0 +1,32 @@
+package lessonSix;
+
+import okhttp3.*;
+import java.io.IOException;
+
+public class WeatherFromAPI {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("295212")
+                .addQueryParameter("apikey", "HdLFGyNLPeQl3AfDDQVkJYGq1uaj3reA")
+                .addQueryParameter("language", "ru")
+                .addQueryParameter("details", "false")
+                .addQueryParameter("metric", "true")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+        String responseBody = response.body().string();
+        System.out.println(responseBody);
+    }
+}
